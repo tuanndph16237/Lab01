b232f45799e70c9571010540efed187910237442
tuanndph16237 <tuanndph16237@example.com>
2022-09-16 23:08:12 +0700

    ex4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component } from '@angular/core';
-import { Inventor } from 'src/interface/Inventor';
+import { IProduct } from 'src/interface/Product';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,30 +8,79 @@ import { Inventor } from 'src/interface/Inventor';
 export class AppComponent implements AfterViewInit{
   title = 'Lab01';
 
-inventors: Inventor[] = [
-{id:1, first:"Albert", last:"Einstein", year: 1879, passed:1955},
-{id:2, first:"Isaac", last:"Newton", year: 1643, passed:1727},
-{id:3, first:"Marie", last:"Curie", year: 1867, passed:1934},
-{id:4, first:"Galileo", last:"Galilei", year: 1564, passed:1642}
-]
+  products: IProduct[]=[
+    {
+    "productId": 1,
+    "productName": "Leaf Rake",
+    "productCode": "GDN-0011",
+    "releaseDate": "March 19, 2016",
+    "description": "Leaf rake with 48-inch wooden handle.",
+    "price": 19.95,
+    "starRating": 3.2,
+    "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    },
+    {
+    "productId": 2,
+    "productName": "Garden Cart",
+"productCode": "GDN-0023",
+"releaseDate": "March 18, 2016",
+"description": "15 gallon capacity rolling garden cart",
+"price": 32.99,
+"starRating": 4.2,
+"imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+},
+{
+"productId": 5,
+"productName": "Hammer",
+"productCode": "TBX-0048",
+"releaseDate": "May 21, 2016",
+"description": "Curved claw steel hammer",
+"price": 8.9,
+"starRating": 4.8,
+"imageUrl": "http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+},
+{
+"productId": 8,
+"productName": "Saw",
+"productCode": "TBX-0022",
+"releaseDate": "May 15, 2016",
+"description": "15-inch steel blade hand saw",
+"price": 11.55,
+"starRating": 3.7,
+"imageUrl": "http://openclipart.org/image/300px/svg_to_png/27070/egore911_saw.png"
+},
+{
+"productId": 10,
+"productName": "Video Game Controller",
+"productCode": "GMG-0042",
+"releaseDate": "October 15, 2015",
+"description": "Standard two-button video game controller",
+"price": 35.95,
+"starRating": 4.6,
+"imageUrl": "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
+}
+];
 ngAfterViewInit(): void {
   this.render(this.show);
 }
 show(innerHTML:string){
-  let item =document.querySelector('#inventor');
+  let item =document.querySelector('#products');
 
   if (item){
     item.innerHTML = innerHTML;
   }
   }
   render(callback: (data: string)=> any){
-  let innerHTML= this.inventors.map(inventor=>{
+  let innerHTML= this.products.map(products=>{
   return `<tr>
-  <td>${inventor.id}</td>
-  <td>${inventor.first}</td>
-  <td>${inventor.last}</td>
-  <td>${inventor.year}</td>
-  <td>${inventor.passed}</td>
+  <td>${products.productId}</td>
+  <td>${products.productName}</td>
+  <td>${products.productCode}</td>
+  <td>${products.releaseDate}</td>
+  <td>${products.description}</td>
+  <td>${products.price}</td>
+  <td>${products.starRating}</td>
+  <td>${products.imageUrl}</td>
   </tr>`;
   },'').join('');
   callback(innerHTML)
