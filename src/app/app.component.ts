f05467fd3d2dbb4dde7b964e7e56852827e342d3
tuanndph16237 <tuanndph16237@example.com>
2022-09-16 22:45:59 +0700

    excersise 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,39 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+import { Inventor } from 'src/interface/Inventor';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'Lab01';
-  student={
-    FullName:'Nguyen Duc Tuan',
-  age: 20,
-  Gioitinh:'Nam',
-DateofBirth: '05/10/2002',
-images: '../assets/images/Visual_Studio_Code_1.35_icon.svg.png',
-diemTB:6.0
-  }
 
+inventors: Inventor[] = [
+{id:1, first:"Albert", last:"Einstein", year: 1879, passed:1955},
+{id:2, first:"Isaac", last:"Newton", year: 1643, passed:1727},
+{id:3, first:"Marie", last:"Curie", year: 1867, passed:1934},
+{id:4, first:"Galileo", last:"Galilei", year: 1564, passed:1642}
+]
+ngAfterViewInit(): void {
+  this.render(this.show);
+}
+show(innerHTML:string){
+  let item =document.querySelector('#inventor');
+
+  if (item){
+    item.innerHTML = innerHTML;
+  }
+  }
+  render(callback: (data: string)=> any){
+  let innerHTML= this.inventors.map(inventor=>{
+  return `<tr>
+  <td>${inventor.id}</td>
+  <td>${inventor.first}</td>
+  <td>${inventor.last}</td>
+  <td>${inventor.year}</td>
+  <td>${inventor.passed}</td>
+  </tr>`;
+  },'').join('');
+  callback(innerHTML)
+  }
 }
